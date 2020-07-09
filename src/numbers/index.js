20f454c7515d7376f82d0346624817dedb875483
Meredith Davis <meredithd23@yahoo.com>
2020-07-08 19:18:32 -0500

    all tests passing

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,17 +1,33 @@
+const moment = require('moment')
+
 const isEven = (num) => {
   // write code for numbers.isEven
-
+  if(num% 2 === 0) {
+  return true;
 }
-
+};
 const sum = (arr) => {
   // write code for numbers.sum
-
+  if(arr.length === 1) {
+    return arr[0]
+  } else {
+    return arr.pop() + sum(arr)
+  }
 }
+
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
+  for(let i=0; i<arr.length; i++) {
+    for(let j=0; j<arr.length -i; j++) {
+      if(arr[i] + arr[j] !== sum) {
+        return false 
+      } 
+      }
+    }
+  }
 
-}
+
 
 module.exports = {
   isEven,
